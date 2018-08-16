7fce67a34961f441793d54b3d9c5a690027eb69d
马兴骢 <demonmaxc@gmail.com>
2018-08-16 11:54:12 +0800

    add Icon block

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,10 +1,23 @@
-import * as React from 'react';
+import React, { Component } from 'react'
+import { Switch, FormControlLabel } from "@material-ui/core";
 
-const Icon = () => {
-  return (
-    <main style={{ marginLeft: 300 }}>Icon</main>
-  )
+class Icon extends Component {
+  state = {
+    isShowGrid: false
+  }
+  toggleGrid = event => {
+    this.setState({isShowGrid: !this.state.isShowGrid})
+  }
+  render() {
+    const {isShowGrid} = this.state
+
+    return (
+      <main className="showcase">
+        <FormControlLabel label="Show Grids" 
+          control={<Switch checked={isShowGrid} onChange={this.toggleGrid} />} />
+      </main>
+    )
+  }
 }
 
-export default Icon;
-  
+export default Icon
